4229e3d64e887b3a0d1348cc4a1873966fb95d10
Jess-Windows <jessicasantanacampos@gmail.com>
2023-09-28 15:49:15 -0300

    method gerarNumeroAleatorio e setState p/ tela

--- Modulo 6/Projeto Flutter e Primeiros Componentes/trilhaapp/lib/main.dart
+++ Modulo 6/Projeto Flutter e Primeiros Componentes/trilhaapp/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 // precisamos de um void main() para rodar um aplicativo no dart e no flutter, porem, no flutter ele nao precisa obrigatoriamente de parametros(argumentos)
@@ -29,12 +31,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var numeroGerado = 0;
+
+  int _gerarNumeroAleatorio() {
+    Random aleatorio = Random(); // cria uma variavel(aleatorio) da class Random para criar numeros Random()
+    return aleatorio.nextInt(1000); // vai retornar um numero aleatorio gerado de 0 ate 1000
+  }
+
   @override
   Widget build(BuildContext context) {
+    debugPrint("chamando metodo build"); // utilizado no modo debug
     return Scaffold(
       appBar: AppBar(
-        title: const Text ("Meu App"),
-      ),
+        title:  const Text ("Meu App"),
+      ),body: Center(child: Text(numeroGerado.toString())),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          // setState serve para colocar as variaveis que estarao em constante mutacao e ira atualizar a tela a medida que elas sofrerem atualizacoes
+          setState(() {
+            numeroGerado = _gerarNumeroAleatorio(); // associando numeroGerado a _gerarNumeroAleatorio
+          // debugPrint(numeroGerado.toString()); // exibido toda vez que o metodo build for chamado para atualizar a pagina exibir as atualizacoes da variavel numeroGerado
+          });
+          print(numeroGerado); // mostra o numero que sera gerado e devera ser exibido na tela pelo widget Text
+        }), // o FloatingActionButton precisa de um button(onPressed) e o onPressed precisa de uma funcao
     ); // Container() e um widget com varias propriedades que podem ser colocadas dentro do seu child:
   }
 }
